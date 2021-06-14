5e7aa9d6b0844f1dae42cfb8650a1f16ef65c55d
Joseph <josephsasson1@gmail.com>
2021-06-14 10:58:19 -0400

    commit tree 73bc8f7d7eb7d582c1e75d54639fe9e34ba3f0ee

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(destination){
+  if (destination >= hq){return destination - hq}
+  else if (destination <= hq) {return hq - destination}
+ }
+
+ function distanceFromHqInFeet(destinationByFeet){
+   return distanceFromHqInBlocks(destinationByFeet)*feet
+ }
+
+ function distanceTravelledInFeet(start, end){
+  if (start >= end){return (start - end)*feet}
+  else if (start <= end) {return (end - start)*feet}
+ }
+
+ function calculatesFarePrice(start, end){
+   const travelByFoot = distanceTravelledInFeet(start, end)
+  if (travelByFoot <= 400) {return 0}
+    else if (travelByFoot > 400 && travelByFoot <= 2000) {return (travelByFoot-400) * .02}
+      else if (travelByFoot > 2000 && travelByFoot < 2500){return 25}
+        else {return "cannot travel that far"}
+ }
+ 
